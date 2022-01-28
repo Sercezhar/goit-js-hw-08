7a43b5a1851440ca0162eca0d7226d865d41ab23
Oleksandr Havryliuk <deolis.ua@gmail.com>
2022-01-28 22:48:46 +0200

    task-2: done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const videoRef = document.querySelector('#vimeo-player');
+const player = new Player(videoRef);
+
+const saveCurrentTime = function (time) {
+  localStorage.setItem('videoplayer-current-time', time.seconds);
+  console.log(localStorage.getItem('videoplayer-current-time'));
+};
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+const setSavedTime = function () {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+};
+setSavedTime();
