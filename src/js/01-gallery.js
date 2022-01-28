038b01ee627d4c72c0cd017bc74c4c86b185da7d
Oleksandr Havryliuk <deolis.ua@gmail.com>
2022-01-28 17:29:23 +0200

    task-1: done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const images = galleryItems
+  .map(
+    image =>
+      `<a class="gallery__item" href="${image.original}">
+          <img class="gallery__image" src="${image.preview}" alt="${image.description}"/>
+      </a>`,
+  )
+  .join('');
+
+galleryRef.innerHTML = images;
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
